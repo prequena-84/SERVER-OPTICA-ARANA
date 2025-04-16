fed9972476397e45f7ba9a56e7c5853ec21faf29
prequena-84 <pedrorequenarondon@hotmail.com>
2025-04-16 19:58:25 -0400

    add-feacture-2

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,8 @@ import type { IRequest, IResponse } from 'types/TRouter'
 // Importación Servicios del modulo de Cliente
 import CUSTOMER_REGISTRATION from './routes/router-client-registration'
 import CUSTOMER_UPDATE from './routes/router-client-update'
+import CUSTOMER_DELETE from './routes/router-client-delete'
+import CUSTOMER_DOWNLOAD from './routes/router-client-download'
 
 const CS = new ISR()
 const servidor = CS.Servidor()
@@ -19,5 +21,7 @@ servidor.all('/', (req:IRequest, res:IResponse ) => {
 
 servidor.use("/api-customer-update", CUSTOMER_UPDATE)
 servidor.use("/api-customer-registration", CUSTOMER_REGISTRATION)
+servidor.use("/api-customer-delete", CUSTOMER_DELETE)
+servidor.use("/api-customer-download", CUSTOMER_DOWNLOAD)
 
 servidor.listen(PORT, () => console.log(`Servidor corriendo en: http://localhost:${PORT}`))
